2e1c0964d3947e8e0cdc031aa4bfb7fcf5e96c8d
zsjlu <940142834@qq.com>
2021-02-06 22:57:46 +0800

    更新到第九部分，情人节特供

--- part9/9.3/config/local_setting.py
+++ part9/9.3/config/local_setting.py
@@ -8,5 +8,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql://flask_for_movie:flask_for_movie@47.115.163.10
                           'flask_for_movie'
 
 DOMAIN = {
-    "www": "http://192.168.0.105:5000"
+    "www": "http://192.168.0.105:81"
 }
